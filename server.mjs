d646d7729dd37f0d28504b530ce28011d2063808
gpostVT <gep100iosgames@gmail.com>
2024-10-08 11:30:22 -0400

    Update listen

--- server.mjs
+++ server.mjs
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
 });
 
 const port = 3000;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server is running on http://0.0.0.0:${port}`);
 });
